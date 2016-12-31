d5f55d01731946eff190a16dad030bac70a861b0
hesamkaveh <hk.hesam.hk@gmail.com>
2016-12-31 15:19:06 +0330

    some bugs are fixed and start writing main of program
    i'm start working on main of program to use it on terminal
    we still have some bugs but I've already fixed some

--- main.py
+++ main.py
@@ -15,19 +15,30 @@ class complexNum(object):
         self.vDiffy = diff(self.v, y, 1)
 
     def checkDiff(self):
-        if (self.uDiffx == self.vDiffy and self.uDiffy == -self.vDiffx):
+        self.f1 = Lambda((x, y), self.uDiffx)
+        self.f2 = Lambda((x, y), self.vDiffy)
+        self.f3 = Lambda((x, y), self.uDiffy)
+        self.f4 = Lambda((x, y), -self.vDiffx)
+        if (self.f1 == self.f2 and self.f3 == self.f4):
             return 1
+
+        # elif age
         else:
             return 0
 
     def complexDiff(self):
         complexNum.eachDiff(self)
+        checker = bool(int(input(
+            "to calculate diff on a particular point enter \'0\' or calculate as symbolic number enter \'1\'?")))
+        if checker == 1:
+            # if (1):
+            if (complexNum.checkDiff(self)):
+                return (self.uDiffx + self.vDiffx * I)
+            else:
+                return ("It had'nt condition survey on Cauchy-Riemann")
+        elif checker == 0:
 
-        # if (complexNum.checkDiff(self)):                         because the bug on checkdiff, i ignore to checkdiff
-        if (1):
-            return (self.uDiffx + self.vDiffx * I)
-        else:
-            return ("It had'nt condition survey on Cauchy-Riemann")
+            complexNum.setSymbols(self)
 
     def setSymbols(self):
         self.x = int(input("enter x value: "))
@@ -35,41 +46,63 @@ class complexNum(object):
         f = Lambda((x, y), self.cNum)
         return (f(self.x, self.y))
 
+    def printNum(self):
+        print(self.cNum)
+
     def getSettedSymbolValue(self):
-        complexNum.setSymbol(self)
+        complexNum.setSymbols(self)
         # complexNum.print(self)
 
-    def __mul__(self, other):
-        return (self.u * other.u + self.u * other.v * I + self.v * other.u * I - self.v * other.v)
+        # def __mul__(self, other):
+        #     return (self.u * other.u + self.u * other.v * I + self.v * other.u * I - self.v * other.v)
+        #
+        # def __add__(self, other):
+        #     return (self.u + other.u + self.v * I + other.v * I)
+        #
+        # def __sub__(self, other):
+        #     return (self.u - other.u + self.v * I - other.v * I)  # class normalNum(object):
 
-    def __add__(self, other):
-        return (self.u + other.u + self.v * I + other.v * I)
 
-    def __sub__(self, other):
-        return (self.u - other.u + self.v * I - other.v * I)
-
-
-class normalNum(object):
-    def __init__(self, obj):
-        self.num = obj
-
-    def __mul__(self, other):
-        self.num * other.num
-
-    def __add__(self, other):
-        sef.num + other.num
-
-    def __sub__(self, other):
-        self.num - other.num
-
-    def __rdiv__(self, other):
-        self.num / other.num
+# def __init__(self, obj):
+#         self.num = obj
+#
+#     def __mul__(self, other):
+#         self.num * other.num
+#
+#     def __add__(self, other):
+#         sef.num + other.num
+#
+#     def __sub__(self, other):
+#         self.num - other.num
+#
+#     def __rdiv__(self, other):
+#         self.num / other.num
 
 
 x, y, c = symbols('x y c', real=True)
-a = complexNum(2 * x)
-b = complexNum(3 + 6 * I)
 
-print(a + b)
 
-# bug: Cauchy-Riemann sufficient conditions checker
+# b = sympify(input())
+# print(type(b))
+# b = complexNum(sympify(input()))
+# b.printNum()
+#
+class text:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+whichcase = int(input(text.BOLD + "which proccese do you want to do?\n" + text.ENDC
+                      + text.OKGREEN + "1- differential of normal number\n"
+                                       "2- differential of complex number\n" + text.ENDC
+                      + text.OKBLUE + "3- integral of normal number\n"
+                                      "4- integral of complex number" + text.ENDC))
+
+
+# bug: Cauchy-Riemann sufficient conditions checker: when we should equation the function
